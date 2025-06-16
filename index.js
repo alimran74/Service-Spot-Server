5253e7a8bd32c085637f5b6a417446ef69468b9c
alimran74 <alimranimran748596@gmail.com>
2025-06-16 12:13:52 +0600

    server deployed

--- index.js
+++ index.js
@@ -257,10 +257,10 @@ res.send(categories.map(cat => cat.category));
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
